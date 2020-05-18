bbb56e31d626a53255794f93ecff3dad617a78fa
Frank <franklinewax@gmail.com>
2020-05-18 03:53:33 +0300

    Using jquery to toggle hide and show in what we do section

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function(){
+    $("#design-image").click(function(){
+        $("#design-image").hide();
+        $("#design").show();
+      });
+      $("#design").click(function(){
+        $("#design").hide();
+        $("#design-image").show();
+      });
+      $("#development-image").click(function(){
+        $("#development-image").hide();
+        $("#development").show();
+      });
+      $("#development").click(function(){
+        $("#development").hide();
+        $("#development-image").show();
+      });
+      $("#product-image").click(function(){
+        $("#product-image").hide();
+        $("#product").show();
+      });
+      $("#product").click(function(){
+        $("#product").hide();
+        $("#product-image").show();
+      });
+});
